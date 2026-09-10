1c6bb0595b88e6d9b8809c8c3da06eb03d20ab73
Mohammad Asad <149931864+tomatoo-pie@users.noreply.github.com>
2026-09-10 18:16:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
@@ -1,39 +1,42 @@
 class Solution {
-  public:
+public:
     vector<int> findUnion(vector<int> &a, vector<int> &b) {
-        // code here
-        vector<int> unionarr;
-        int n1 = a.size();
-        int n2 = b.size();
-        int i = 0;
-        int j = 0;
-        while(i < n1 && j < n2){
-            if(a[i]<=b[j]){
-                if(unionarr.size()==0 || unionarr.back()!=a[i]){
-                    unionarr.push_back(a[i]);
-                }
+        vector<int> result;
+        int i = 0, j = 0;
+
+        while (i < a.size() && j < b.size()) {
+            if (a[i] < b[j]) {
+                if (result.empty() || result.back() != a[i])
+                    result.push_back(a[i]);
                 i++;
-            }else{
-                if(unionarr.size()==0 || unionarr.back()!=b[j]){
-                    unionarr.push_back(b[j]);
-                }
+            }
+            else if (a[i] > b[j]) {
+                if (result.empty() || result.back() != b[j])
+                    result.push_back(b[j]);
+                j++;
+            }
+            else { // a[i] == b[j]
+                if (result.empty() || result.back() != a[i])
+                    result.push_back(a[i]);
+                i++;
                 j++;
             }
         }
-        
-        while(i<n1){
-            if(unionarr.size()==0 || unionarr.back()!=a[i]){
-                    unionarr.push_back(a[i]);
-                }
-                i++;
+
+        // Remaining elements of a
+        while (i < a.size()) {
+            if (result.empty() || result.back() != a[i])
+                result.push_back(a[i]);
+            i++;
         }
-        
-        while(j<n2){
-            if(unionarr.size()==0 || unionarr.back()!=b[j]){
-                    unionarr.push_back(b[j]);
-                }
-                j++;
+
+        // Remaining elements of b
+        while (j < b.size()) {
+            if (result.empty() || result.back() != b[j])
+                result.push_back(b[j]);
+            j++;
         }
-        return unionarr;
+
+        return result;
     }
 };
